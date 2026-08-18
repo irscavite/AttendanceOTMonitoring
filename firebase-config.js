1eab81810345ebef2b4e941f568d0af76723be47
irscavite <irscavite@gmail.com>
2026-08-18 12:44:23 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase Web configuration for the OT Monitoring system.
+// Project: otmonitoring
+window.OT_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAPvKUwUvEVJ96oiI0ZhI7WsmzsSBEkGyk",
+  authDomain: "otmonitoring.firebaseapp.com",
+  databaseURL: "https://otmonitoring-default-rtdb.firebaseio.com",
+  projectId: "otmonitoring",
+  storageBucket: "otmonitoring.firebasestorage.app",
+  messagingSenderId: "243995453146",
+  appId: "1:243995453146:web:9fe4281a03803b6a68a7e3",
+  measurementId: "G-0H9VT0JKP3"
+};
